b272cd1018bd47919009ad80c9402208f6b91c57
Musca Mihail <muscamihaill@gmail.com>
2026-09-21 15:18:49 +0300

    feat: remove old list profiles script, added useful nix shortcuts

--- core/pkgs/rig/cli.py
+++ core/pkgs/rig/cli.py
@@ -1,6 +1,35 @@
+import subprocess
 import typer
 
 rig = typer.Typer(
     name="rig",
     add_completion=False,
 )
+
+nix = typer.Typer(name="nix")
+rig.add_typer(nix)
+
+@nix.command()
+def rebuild():
+    subprocess.run(
+        ["sudo", "nixos-rebuild", "switch"],
+        check=True,
+    )
+
+@nix.command()
+def upgrade():
+    subprocess.run(
+        ["sudo", "nix", "flake", "update"],
+        check=True,
+    )
+    rebuild()
+
+@nix.command()
+def profiles():
+    result = subprocess.run(
+        ["sudo", "nix-env", "--list-generations", "--profile", "/nix/var/nix/profiles/system"],
+        check=True,
+        capture_output=True,
+        text=True,
+    )
+    print(result.stdout)
--- scripts/list-profiles.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-sudo nix-env --list-generations --profile /nix/var/nix/profiles/system
